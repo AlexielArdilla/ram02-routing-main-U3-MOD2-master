52564f4b92ba928d387ab9d34cf4c960cd7d7233
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-04-18 19:07:28 -0300

    primer commit

---
+++ src/ram/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../auth/components/styles-components.css'
+
+export const NotFound = () => {
+  return (
+    <div className='contenedor404'>
+      <h1>Error 404</h1>
+      <p>Página no encontrada</p>
+    </div>
+  )
+}
